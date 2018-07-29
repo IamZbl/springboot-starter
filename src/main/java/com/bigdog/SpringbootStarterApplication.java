106b75e072f925282f5808bad05e7cca313d2321
IamZbl <475749368@qq.com>
2018-07-30 00:38:16 +0800

    修改

--- src/main/java/com/bigdog/SpringbootStarterApplication.java
+++ src/main/java/com/bigdog/SpringbootStarterApplication.java
@@ -1,4 +1,4 @@
-package com.bigdog.springbootstarter;
+package com.bigdog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
