e1ce74e4f0a378670ae80994ecbd52b3e84356fc
IamZbl <475749368@qq.com>
2018-07-31 12:40:57 +0800

    JSONResult

--- src/main/java/com/bigdog/controller/HelloController.java
+++ src/main/java/com/bigdog/controller/HelloController.java
@@ -3,6 +3,9 @@ package com.bigdog.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bigdog.pojo.JSONResult;
+import com.bigdog.pojo.User;
+
 @RestController
 public class HelloController {
 
@@ -10,6 +13,16 @@ public class HelloController {
 	public 	Object hello()
 	{
 		return "helloe springboot";
+		
+	}
+	
+	@RequestMapping("/getUser")
+	public JSONResult getUser()
+	{
+		User user = new User();
+		user.setAge(27);
+		user.setName("张博伦");
+		return JSONResult.ok(user);
 		
 	}
 }
