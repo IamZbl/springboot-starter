ae6bdbc9b2c8a18fee8cff5d2b62f2411627ad38
IamZbl <475749368@qq.com>
2018-07-31 18:37:58 +0800

    修改server默认配置

--- src/main/java/com/bigdog/controller/HelloController.java
+++ src/main/java/com/bigdog/controller/HelloController.java
@@ -1,14 +1,20 @@
 package com.bigdog.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bigdog.pojo.DogResource;
 import com.bigdog.pojo.JSONResult;
 import com.bigdog.pojo.User;
 
 @RestController
 public class HelloController {
 
+	@Autowired
+	private DogResource dogResource;
+	
 	@RequestMapping("/hello")
 	public 	Object hello()
 	{
@@ -24,5 +30,13 @@ public class HelloController {
 		user.setName("张博伦");
 		return JSONResult.ok(user);
 		
+	}
+	
+	@RequestMapping("/getResource")
+	public JSONResult getResource()
+	{
+		DogResource r = new DogResource();
+		BeanUtils.copyProperties(dogResource, r);
+		return JSONResult.ok(r);
 	}
 }
